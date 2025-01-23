891f7a9c2b5bf11f60dba1977fac7feed8719768
AliciaNazar <alicianazarab@gmail.com>
2025-01-22 22:35:55 -0300

    documentacion

--- src/main/java/com/mindhub/product_microservice/controllers/ProductController.java
+++ src/main/java/com/mindhub/product_microservice/controllers/ProductController.java
@@ -3,6 +3,9 @@ package com.mindhub.product_microservice.controllers;
 import com.mindhub.product_microservice.dtos.ProductDTO;
 import com.mindhub.product_microservice.dtos.ProductDTOResquest;
 import com.mindhub.product_microservice.services.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,18 +20,33 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @Operation(summary = "Get all products", description = "Retrieve a list of all products.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Products retrieved successfully."),
+    })
     @GetMapping("/products")
     public ResponseEntity<List<ProductDTO>> getProducts(){
         List<ProductDTO> products = this.productService.getProducts();
         return ResponseEntity.ok(products);
     }
 
+    @Operation(summary = "Create a new product", description = "Registers a new product in the system.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Product created successfully."),
+            @ApiResponse(responseCode = "400", description = "Invalid input data."),
+    })
     @PostMapping("/products")
     public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTOResquest productDTOResquest){
         ProductDTO productDTO = this.productService.createProduct(productDTOResquest);
         return ResponseEntity.status(HttpStatus.CREATED).body(productDTO);
     }
 
+    @Operation(summary = "Update the stock of a product.", description = "Updates the stock of a specific product identified by its ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Stock updated successfully."),
+            @ApiResponse(responseCode = "404", description = "Product not found"),
+            @ApiResponse(responseCode = "400", description = "Invalid request")
+            })
     @PutMapping("/products/{id}")
     public ResponseEntity<ProductDTO> updateProductStock(
             @PathVariable("id") Long id,
@@ -37,6 +55,11 @@ public class ProductController {
         return ResponseEntity.ok(productUpdated);
     }
 
+    @Operation(summary = "Delete a product by ID", description = "Deletes an existing product by the provided ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Product deleted successfully."),
+            @ApiResponse(responseCode = "404", description = "Product not found.")
+    })
     @DeleteMapping("products/{id}")
     public ResponseEntity<?> deleteProduct(@PathVariable("id") Long id){
         this.productService.deleteProduct(id);
